ed7367c58850d725359dc6f9c015fa0893253d3f
Rob Bowden <rbowden91@gmail.com>
2019-01-30 03:34:48 -0500

    add in osx installation steps

--- install.sh
+++ install.sh
@@ -1,20 +1,8 @@
 #!/usr/bin/env sh
 # tested on Ubuntu 18.04
+# developed in but not re-run in mojave 1/30/2019
 
-# mono takes a long time to install
-# add PPA for Mono
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
-sudo apt-get update
-
-sudo apt-get install -y curl
-
-# install rust for YouCompleteMe
-curl https://sh.rustup.rs -sSf > /tmp/rust.sh
-sh /tmp/rust.sh -y
-source ~/.cargo/env
-rm -f /tmp/rust.sh
-
+# Dependencies:
 # build-essential, cmake, python3-dev: YouCompleteMe (https://github.com/Valloric/YouCompleteMe)
 # nodejs, npm: YouCompleteMe JavaScript/TypeScript support
 # clang, clang-tidy: YouCompleteMe C family support
@@ -23,32 +11,75 @@ rm -f /tmp/rust.sh
 # openjdk-8-jre: YouCompleteMe Java support
 # ack: ack.vim (https://github.com/mileszs/ack.vim)
 # codequery: vim-codequery (https://github.com/devjoe/vim-codequery)
-sudo apt-get install -y \
-	build-essential \
-	clang \
-	clang-tidy \
-	python2.7 \
-	python-pip \
-	python3 \
-	python3-pip \
-	ruby \
-	python3-dev \
-	python-dev  \
-	git \
-	libncurses-dev \
-	nodejs \
-	npm \
-	cmake \
-	ctags \
-	ack \
-	codequery \
-	golang-go \
-	mono-complete \
-	openjdk-8-jre
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+	# xcode cli tools need to be installed, as does brew
+
+	# brew install python3 needs this to exist
+	sudo mkdir -p /usr/local/Frameworks
+	sudo chown rbowden /usr/local/Frameworks
+
+	# couldn't find clang-tidy, *-dev, openjdk-8-jre
+	brew install ack python3 nodejs ruby cmake ctags codequery golang mono
+
+	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+	python3 get-pip.py
+	rm get-pip.py
+	PIPPATH=$(find /usr/local/Cellar/python/*/Frameworks/Python.framework/Versions/* -name bin)
+	export PATH="$PIPPATH:$PATH"
+	echo 'export PATH="'"$PIPPATH"':$PATH"' >> ~/.bashrc
+
+	
+
+elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+
+	# mono takes a long time to install
+	# add PPA for Mono
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+	echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+	sudo apt-get update
+
+	sudo apt-get install -y curl
+
+	sudo apt-get install -y \
+		build-essential \
+		clang \
+		clang-tidy \
+		python2.7 \
+		python-pip \
+		python3 \
+		python3-pip \
+		ruby \
+		python3-dev \
+		python-dev  \
+		git \
+		libncurses-dev \
+		nodejs \
+		npm \
+		cmake \
+		ctags \
+		ack \
+		codequery \
+		golang-go \
+		mono-complete \
+		openjdk-8-jre
+
+elif [[ "$OSTYPE" == "freebsd"* ]]; then
+        # ...
+else
+        # Unknown.
+fi
+
+# install rust for YouCompleteMe
+curl https://sh.rustup.rs -sSf > /tmp/rust.sh
+sh /tmp/rust.sh -y
+source ~/.cargo/env
+rm -f /tmp/rust.sh
 
 # https://github.com/junegunn/fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install -y
+~/.fzf/install --key-bindings --completion --update-rc
 
 pip install pycscope
 sudo gem install starscope
@@ -70,10 +101,11 @@ cd /tmp/vim
 	    --with-features=huge
 make
 sudo make install
+rm -rf /tmp/vim
 
 # download actual vimrc files
 cd ~
-rm -rf /tmp/vim ~/.vim ~/.vimrc
+rm -rf ~/.vim ~/.vimrc
 git clone https://github.com/rbowden91/vimrc.git ~/.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 ln -s ~/.vim/.vimrc ~/.vimrc
@@ -81,4 +113,5 @@ vim +PluginInstall +qall
 
 # finish YouCompleteMe installation
 cd ~/.vim/bundle/YouCompleteMe
+# TODO: osx doesn't have java-completer
 ./install.py --cs-completer --go-completer --rust-completer --java-completer --clang-completer
